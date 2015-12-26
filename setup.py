df9dd1c0790789f879b8a1f5cbad2d755b15f322
Alberto Berti <alberto@metapensiero.it>
2015-12-26 16:43:18 +0100

    Fix packaging

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 
     license="GPLv3+",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
